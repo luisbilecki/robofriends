b7cda4e1c42179d763878668353d52eadd8b7c60
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-12-19 16:27:52 -0300

    Add redux store and provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,6 @@
-import { CHANGE_SEARCHFIELD } from '../constants';
+import { combineReducers } from 'redux';
+import { searchRobots } from './robots';
 
-const initialState = {
-    searchField: '',
-};
-
-export const searchRobots = (state = initialState, action={}) => {
-    switch (action.type) {
-        case CHANGE_SEARCHFIELD:
-            return Object.assign({}, state, { searchField: action.payload });
-        default:
-            return state;
-    }
-};
+export default combineReducers({
+    robots: searchRobots,
+})
