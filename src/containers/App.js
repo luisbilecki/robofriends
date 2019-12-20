b4ed7e8d08a9dbfe4b2e629d49ae8196ae303072
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-12-20 14:04:08 -0300

    Create header component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -28,7 +29,7 @@ class App extends Component {
             ) :        
             (
                 <div className='tc'>
-                    <h1 className='f2'>RoboFriends</h1>
+                    <Header />
                     <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundary>
