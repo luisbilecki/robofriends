88a20ba2e7e7ccb8d26044a014515a6dafff3c36
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-12-23 18:04:13 -0300

    Add test for ErrorBondary component

---
+++ src/components/ErrorBoundary/index.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ErrorBoundary from './';
+
+it('expect to render ErrorBoundary component', () => {    
+    expect(shallow(<ErrorBoundary />)).toMatchSnapshot();
+});
